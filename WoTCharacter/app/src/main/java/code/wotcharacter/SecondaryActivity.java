33f4a6cb499574892f28f30366a4cea0cdb15628
MrFizban <liampreto@gmail.com>
2019-04-26 13:36:12 +0200

    Add files via upload

---
+++ WoTCharacter/app/src/main/java/code/wotcharacter/SecondaryActivity.java
@@ -0,0 +1,15 @@
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+package code.wotcharacter;
+
+public class SecondaryActivity extends AppCompatActivity{
+
+
+}
